5dc176e402c826e4681ad89828947dd213230faa
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-08 12:15:48 +0530

    added login api

--- src/controllers/auth/userLogin.js
+++ src/controllers/auth/userLogin.js
@@ -1,3 +1,37 @@
+const bcrypt = require("bcrypt");
+const User = require("../../models/User");
+
 module.exports = async function userLogin(req, res, next) {
-    return (res.status(200).json('success'));
-}
+  const { username, password } = req.body;
+
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    // If the user doesn't exist, return an error
+    if (!user) {
+      return res.status(401).json({ Error: "Invalid username or password." });
+    }
+
+    // Compare the provided password with the hashed password stored in the database
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    // If the passwords match, the login is successful
+    if (isPasswordValid) {
+      return res.json({
+        data: {
+          userId: user._id,
+          username: user.username,
+          email: user.email,
+        },
+        message: "Login successful.",
+      });
+    } else {
+      // If the passwords don't match, return an error
+      return res.status(401).json({ Error: "Invalid username or password." });
+    }
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
