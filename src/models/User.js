e0ee764a797fc062f562803b78782b9b2431396e
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-08 11:52:55 +0530

    revert column change

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,6 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId,
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
