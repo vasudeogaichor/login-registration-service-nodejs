79fba86de3cf05be576c9ba399a82c8d60fd0625
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-14 16:19:29 +0530

    configre adding and removing of likes

--- src/controllers/post/handleLikes.js
+++ src/controllers/post/handleLikes.js
@@ -1,3 +1,59 @@
+const Post = require("../../models/Post");
+
 module.exports = async function handleLikes(req, res, next) {
-    return {"message": "success"}
-}
+  try {
+    const { postId } = req.params;
+    const { userId, action } = req.body;
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (action === "add") {
+      if (!post.likes.includes(req.body.userId)) {
+        post.likes.push(req.body.userId);
+        await post.save();
+      }
+
+      return res.status(201).json({
+        message: "Like added successfully",
+        data: {
+          postId: post._id,
+          userId: post.userId,
+          content: post.content,
+          likes: post.likes,
+          comments: post.comments,
+        },
+      });
+    } else if (action === "remove" && userId) {
+      const userLikeIndex = post.likes.indexOf(userId);
+
+      if (userLikeIndex !== -1) {
+        post.likes.splice(userLikeIndex, 1);
+        await post.save();
+
+        return res.status(201).json({
+          message: "Like removed successfully",
+          data: {
+            postId: post._id,
+            userId: post.userId,
+            content: post.content,
+            likes: post.likes,
+            comments: post.comments,
+          },
+        });
+      }
+
+      return res.status(400).json({
+        message: "Error: Like already removed/ does not exist"
+      })
+    } else {
+      return res.status(400).json({
+        message: "Error: Wrong action type or userId missing",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
