b24360980a6534adcf2cb3d88d0e884c9f9bfdf3
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-14 13:34:05 +0530

    added post list api

---
+++ src/controllers/post/postList.js
@@ -0,0 +1,36 @@
+const Post = require("../../models/Post");
+
+module.exports = async function postList(req, res, next) {
+  try {
+    console.log(req.params);
+    const { page, limit } = req.params;
+
+    const defaultPage = 1;
+    const defaultLimit = 10;
+
+    skip = ((page || defaultPage) - 1) * (limit || defaultLimit);
+
+    const posts = await Post.find()
+      .skip(skip)
+      .limit(limit || defaultLimit);
+
+    const totalPosts = await Post.countDocuments();
+
+    res.status(200).json({
+      message: "Posts fetched successfully",
+      total: (limit || defaultLimit),
+      actualTotal: totalPosts,
+      data: posts.map((post) => {
+        return {
+          postId: post._id,
+          userId: post.userId,
+          content: post.content,
+          likes: post.likes,
+          comments: post.comments,
+        };
+      }),
+    });
+  } catch (error) {
+    res.status(500).json({ Error: "Internal Server Error" });
+  }
+};
