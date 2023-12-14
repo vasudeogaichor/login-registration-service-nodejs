27b60375bc216e730ff482c1a5bd05baedd3a102
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-14 13:27:45 +0530

    added post create controller

---
+++ src/controllers/post/index.js
@@ -0,0 +1,13 @@
+const postCreate = require("./postCreate");
+const postGet = require("./postGet");
+const postList = require("./postList");
+const postUpdate = require("./postUpdate");
+const postDelete = require("./postDelete");
+
+module.exports = {
+  postCreate,
+  postGet,
+  postList,
+  postUpdate,
+  postDelete,
+};
