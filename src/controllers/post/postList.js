7ab126ee6a0c090cbfcbda6fda9e82a2522bb440
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-14 17:17:50 +0530

    added filters and sorting in list api

--- src/controllers/post/postList.js
+++ src/controllers/post/postList.js
@@ -2,7 +2,7 @@ const Post = require("../../models/Post");
 
 module.exports = async function postList(req, res, next) {
   try {
-    let { page, limit } = req.query;
+    let { page, limit, userId, query, sortBy } = req.query;
 
     const defaultPage = 1;
     const defaultLimit = 10;
@@ -12,8 +12,48 @@ module.exports = async function postList(req, res, next) {
 
     skip = (page - 1) * limit;
 
-    const posts = await Post.find().skip(skip).limit(limit);
+    const filters = {}
 
+    // return posts by specific user
+    if (userId) {
+        filters.userId = userId
+    }
+
+    // search posts using content text
+    if (query?.trim()?.length) {
+        filters.content = { $regex: query, $options: "i" };
+    }
+
+    const sortOptions = {}
+
+    if (sortBy) {
+        let direction = 1;
+        // Direction of sort order decided by the - at the start of sortBy type
+        if (sortBy.startsWith("-")) {
+            direction = -1
+            sortBy = sortBy.slice(1)
+        }
+
+        switch (sortBy) {
+          case "likes":
+            sortOptions.likes = direction;
+            break;
+          case "comments":
+            sortOptions.comments = direction;
+            break;
+          case "createdAt":
+            sortOptions.createdAt = direction;
+            break;
+          default:
+            // Handle other cases or invalid sortBy values
+            break;
+        }
+      } else {
+        // Default sorting (e.g., by createdAt)
+        sortOptions.createdAt = -1;
+      }
+
+    const posts = await Post.find(filters).skip(skip).limit(limit).sort(sortOptions);
     const totalPosts = await Post.countDocuments();
 
     res.status(200).json({
@@ -31,6 +71,7 @@ module.exports = async function postList(req, res, next) {
       }),
     });
   } catch (error) {
+    console.log(error)
     res.status(500).json({ Error: "Internal Server Error" });
   }
 };
