ca173ccaca6d83da22e1ac8390bf7f10bc7155f4
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-14 16:31:39 +0530

    fix pagination in list api

--- src/controllers/post/postList.js
+++ src/controllers/post/postList.js
@@ -2,22 +2,23 @@ const Post = require("../../models/Post");
 
 module.exports = async function postList(req, res, next) {
   try {
-    const { page, limit } = req.params;
+    let { page, limit } = req.query;
 
     const defaultPage = 1;
     const defaultLimit = 10;
 
-    skip = ((page || defaultPage) - 1) * (limit || defaultLimit);
+    page = parseInt(page) || defaultPage;
+    limit = parseInt(limit) || defaultLimit;
 
-    const posts = await Post.find()
-      .skip(skip)
-      .limit(limit || defaultLimit);
+    skip = (page - 1) * limit;
+
+    const posts = await Post.find().skip(skip).limit(limit);
 
     const totalPosts = await Post.countDocuments();
 
     res.status(200).json({
       message: "Posts fetched successfully",
-      total: (limit || defaultLimit),
+      total: limit,
       actualTotal: totalPosts,
       data: posts.map((post) => {
         return {
