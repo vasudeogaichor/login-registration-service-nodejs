0e6a439291952ff21afe26c9fd412fe9f2c0daf4
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-20 00:26:20 +0530

    avoid server halt because of unhandled exceptions

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('express-async-errors');
 
 const { postRouter, authRouter } = require('./routes');
 const {verifyToken} = require('./middleware/authMiddleware')
@@ -16,6 +17,13 @@ app.use(bodyParser.json());
 app.use('/auth', authRouter);
 app.use(verifyToken)
 app.use('/posts', postRouter);
+
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    const stackTrace = err.stack.split('\n');
+    res.status(500).json({Error: stackTrace});
+});
 
 mongoose.connect('mongodb://localhost:27017/db_auth');
 
