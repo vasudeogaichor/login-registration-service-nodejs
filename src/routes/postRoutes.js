b0c9198e4bb6905c59aee1ac5ce0fba430ebad03
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-19 15:34:02 +0530

    return encrypted data in PUT api

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const { postControllers } = require("../controllers");
 const { encryptResponseData, sendEncryptedResponse } = require("../middleware/encryptionMiddleware")
 
-router.post("/", postControllers.postCreate);
-router.get("/", postControllers.postList);
+router.post("/", postControllers.postCreate, encryptResponseData, sendEncryptedResponse);
+router.get("/", postControllers.postList, encryptResponseData, sendEncryptedResponse);
 router.get("/:postId", postControllers.postGet, encryptResponseData, sendEncryptedResponse);
-router.put("/:postId", postControllers.postUpdate);
+router.put("/:postId", postControllers.postUpdate, encryptResponseData, sendEncryptedResponse);
 router.delete("/:postId", postControllers.postDelete);
 
-router.post("/:postId/likes", postControllers.handleLikes)
-router.post("/:postId/comments", postControllers.handleComments)
+router.post("/:postId/likes", postControllers.handleLikes, encryptResponseData, sendEncryptedResponse)
+router.post("/:postId/comments", postControllers.handleComments, encryptResponseData, sendEncryptedResponse)
 
 module.exports = router;
