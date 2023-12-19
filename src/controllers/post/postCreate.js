450ebfe1bb3a33066eda0a0832ff37750188a7c4
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2023-12-19 15:29:11 +0530

    return encrypted data in post api

--- src/controllers/post/postCreate.js
+++ src/controllers/post/postCreate.js
@@ -4,24 +4,23 @@ module.exports = async function postCreate(req, res, next) {
   const { userId, content } = req.body;
 
   try {
-
-    // Create a new post
     const post = new Post({ userId, content });
 
-    // Save the post to the database
     await post.save();
 
-    // Respond with a success message
-    res.status(201).json({
+    res.locals.json = {
       message: "Post created successfully.",
       data: {
         postId: post._id,
         userId: post.userId,
         content: post.content,
       },
-    });
+    };
+
+    res.status(201);
   } catch (error) {
     console.error("Error during post creation:", error);
     res.status(500).json({ Error: "Internal Server Error" });
   }
+  next();
 };
